bb1e8287d4b88af337d6e1c5b82d0578a2918efc
Laís Rhianne <lays.rhianne@gmail.com>
2021-10-06 19:20:35 -0300

    Vuetify added

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,19 @@
 <template>
-  <div id="app">
-    <Home/>
-  </div>
+  <v-app>
+    <v-main>
+      <Home/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Home from './pages/Home.vue'
+import Home from './pages/Home.vue';
 
 export default {
   name: 'App',
+
   components: {
-    Home
-  }
-}
+    Home,
+  },
+};
 </script>
-
-<style>
-* {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    box-sizing: border-box;
-  }
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-main>
+    <input type="text">
+    <div id="submit-button"></div>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data: function() {
+    return {
+      inputValue: ''
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
