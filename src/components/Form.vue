a455968b4103b32013e00e64590a258a53614990
Laís Rhianne <lays.rhianne@gmail.com>
2021-10-06 21:48:29 -0300

    validation added

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,10 +1,25 @@
 <template>
-  <v-container id="form-container">
-    <v-text-field id="task-input" v-model="taskName" label="Task" clearable clear-icon="fas fa-times"/>
-    <v-btn id="submit-btn" color="primary" elevation="5" fab @click="emitAddTaskEvent">
+<v-container id="form-container">
+  <v-form id="form" @submit.prevent="emitAddTaskName">
+    <v-text-field
+      id="task-input"
+      label="Task"
+      v-model="taskName"
+      :rules="[rules.required]"
+      clearable
+      clear-icon="fas fa-times"
+    />
+    <v-btn
+      id="submit-btn"
+      color="primary"
+      elevation="5"
+      fab
+      @click="emitAddTaskEvent"
+    >
       <v-icon>fas fa-plus</v-icon>
     </v-btn>
-  </v-container>
+  </v-form>
+</v-container>
 </template>
 
 <script>
@@ -15,22 +30,35 @@ export default {
   data: function () {
     return {
       taskName: '',
+      rules: {
+        required: value => !!value || 'Required.'
+      },
     };
   },
   methods: {
     emitAddTaskEvent() {
-      this.taskName = ''
-      this.$emit('addTask', { taskName: this.taskName, id: uuidv4(), done: false });
-    }
-  }
+      this.taskName = '';
+      this.$emit('addTask', {
+        taskName: this.taskName,
+        id: uuidv4(),
+        done: false,
+      });
+    },
+  },
 };
 </script>
 
 <style>
 #form-container {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#form {
+  display: flex;
   flex-direction: row;
-  width: 60%;
+  width: 70%;
   margin-top: 30px;
 }
 
